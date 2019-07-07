a16450c31b99889a85f80fe82bfcbcdd40158028
bgpat <bgpat@users.noreply.github.com>
2019-07-07 10:50:14 +0900

    Create jn api
    kubebuilder create api --group jsn --version v1beta1 --kind JobNotifier

--- main.go
+++ main.go
@@ -19,6 +19,8 @@ import (
 	"flag"
 	"os"
 
+	jsnv1beta1 "github.com/bgpat/job-slack-notifier/api/v1beta1"
+	"github.com/bgpat/job-slack-notifier/controllers"
 	"k8s.io/apimachinery/pkg/runtime"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -33,6 +35,7 @@ var (
 
 func init() {
 
+	jsnv1beta1.AddToScheme(scheme)
 	// +kubebuilder:scaffold:scheme
 }
 
@@ -56,6 +59,14 @@ func main() {
 		os.Exit(1)
 	}
 
+	err = (&controllers.JobNotifierReconciler{
+		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("JobNotifier"),
+	}).SetupWithManager(mgr)
+	if err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "JobNotifier")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
